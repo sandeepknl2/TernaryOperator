34e5c890b0aa9e03fdd95e4d325a7c3c0cfcd2f0
sandeepknl2 <161841885+sandeepknl2@users.noreply.github.com>
2024-03-17 11:33:13 +0530

    Update TernaryOperator.java

--- TernaryOperator.java
+++ TernaryOperator.java
@@ -7,3 +7,13 @@ class TernaryOperator {
 	
 	}
 }	
+------------------------------------
+class Ternary {
+	public static void main(String[] args) {
+	int a=10;
+	int b=20;
+	int result = largest a>b ?a:b;
+	System.out.println(result);
+
+	}
+}
